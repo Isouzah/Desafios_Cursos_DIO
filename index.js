3428e0c13b51500c2f06d7ca0587f82ec653bfcd
Isouzah <igsouzah@gmail.com>
2025-01-22 22:59:33 -0300

    Finalização do Desafio

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log ("olá mundo")
+let nome = "Neutrun";
+let xp = 5570;
+let nível;
+
+if (xp <= 1000) {
+    nível = "Ferro";
+}
+else if  (xp > 1000 && xp <= 2000) {
+    nível = "Bronze";
+}
+else if  (xp > 2000 && xp <= 5000) {
+    nível = "Prata";
+}
+else if  (xp > 5000 && xp <= 7000) {
+    nível = "Ouro";
+}
+else if  (xp > 7000 && xp <= 8000) {
+    nível = "Platina";
+}
+else if  (xp > 8000 && xp <= 9000) {
+    nível = "Ascendente";
+}
+else if  (xp > 9000 && xp <= 10000) {
+    nível = "Imortal";
+}
+else if  (xp > 10000) {
+    nível = "Radiante";
+}
+console.log("O Herói de nome " +nome +" está no nível de " +nível)
