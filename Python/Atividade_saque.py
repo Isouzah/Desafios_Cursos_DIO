a5d783591e76bbfda1240955e3dae7f147e767d4
Isouzah <igsouzah@gmail.com>
2025-06-17 22:42:07 -0300

    cheque especial com confirmação

--- Python/Atividade_saque.py
+++ Python/Atividade_saque.py
@@ -1,4 +1,4 @@
-saldo = 5000
+saldo = 500
 print("Seu saldo atual é:", saldo)
 
 sacar = int(input("Digite o valor que deseja sacar: "))
@@ -9,4 +9,15 @@ if sacar <= saldo:
 else:
     print("Saldo insuficiente para realizar o saque.")
     print("Seu saldo atual é:", saldo)
-    print("Tente novamente com um valor menor ou igual ao seu saldo.")
+    print("gostaria de usar o cheque especial? (S/N)")
+    cheque_especial = input().strip().upper()
+    if cheque_especial == 'S':
+        print("realmente deseja usar o cheque especial? (S/N)")
+        confirmacao = input().strip().upper()
+        if confirmacao == 'S':
+            print("Cheque especial ativado.")
+            print("Seu novo saldo é:", saldo - sacar)
+        else:
+            print("Cheque especial não ativado. Saque cancelado.")
+            print("Saque não realizado. Seu saldo permanece:", saldo)
+    print("Saque não realizado. Seu saldo permanece:", saldo)
