f3c6245b8c7cdfa8a098b969ec586ce9e0bd9e24
Isouzah <igsouzah@gmail.com>
2025-06-19 20:23:33 -0300

    primeira ideia de montagem

---
+++ Password_Generator.py
@@ -0,0 +1,8 @@
+#def password_generator(lenght, upper_case, lower_case, numbers, special_characters):
+#    length = int(input("Quantos digitos deseja que a senha tenha? "))
+#    upper_case = bool(input("deseja adiconar letras maiusculas? (S/N) "))
+#    lower_case = bool(input("deseja adiconar letras minusculas? (S/N) "))
+#    numbers = bool(input("deseja adiconar numeros? (S/N)) "))
+#    special_characters = bool(input("deseja adiconar caracteres especiais? (S/N) "))
+
+print("Bem vindo ao gerador de senhas!")
